25b8261aa806bd3ece1948004b10b66e346893c4
Dawid <dawid.marcinkow@gmail.com>
2018-03-02 23:01:07 +0100

    First commit

---
+++ Backend/src/main/java/com/basketballstats/basketballStatistics/BasketballStatisticsApplication.java
@@ -0,0 +1,12 @@
+package com.basketballstats.basketballStatistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasketballStatisticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasketballStatisticsApplication.class, args);
+	}
+}
