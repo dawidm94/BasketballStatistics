33f11bb61849a3041f0d221a5cf3d640b41ea484
Dawid <dawid.marcinkow@gmail.com>
2018-03-05 22:34:19 +0100

    Registration view

--- Frontend/src/app/public/login/login.component.ts
+++ Frontend/src/app/public/login/login.component.ts
@@ -7,12 +7,25 @@ import { Component, OnInit, Renderer2 } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+	showRegistration: boolean = false;
+	showLogin: boolean = true;
+
   constructor(private renderer: Renderer2) {
 	  this.renderer.setStyle(document.body, 'background', 'url("assets/images/login-background.jpg") no-repeat  fixed');
 	  this.renderer.setStyle(document.body, '-webkit-background-size', 'cover');
 	  this.renderer.setStyle(document.body, '-moz-background-size', 'cover');
 	  this.renderer.setStyle(document.body, '-o-background-size', 'cover');
 	  this.renderer.setStyle(document.body, 'background-size', 'cover');
+  }
+
+  registration(){
+	  this.showLogin = false;
+	  this.showRegistration = true;
+  }
+
+  login(){
+	  this.showRegistration = false;
+	  this.showLogin = true;
   }
 
   ngOnInit() {
