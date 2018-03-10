5de6d4c9557a4171f177d77ccdcdd49b32022d8b
Dawid <dawid.marcinkow@gmail.com>
2018-03-10 16:16:30 +0100

    Connection with database and first entity

---
+++ Backend/src/main/java/com/basketballstats/basketballStatistics/models/User.java
@@ -0,0 +1,48 @@
+package com.basketballstats.basketballStatistics.models;
+
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "bs_users")
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "USR_ID")
+	private long id;
+
+	@Column(name = "USR_LOGIN")
+	private String login;
+
+	@Column(name = "USR_PASSWORD")
+	private String password;
+
+	@Column(name = "USR_EMAIL")
+	private String email;
+
+	@Column(name = "USR_FIRSTNAME")
+	private String firstName;
+
+	@Column(name = "USR_LASTNAME")
+	private String lastName;
+
+	@Column(name = "USR_CITY")
+	private String city;
+
+	@Column(name = "USR_COUNTRY")
+	private String country;
+
+	@Column(name = "USR_ACTIVE")
+	private byte active;
+
+}
--- Frontend/src/app/public/login/login.component.ts
+++ Frontend/src/app/public/login/login.component.ts
@@ -1,34 +1,40 @@
 import { Component, OnInit, Renderer2 } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+	selector: 'app-login',
+	templateUrl: './login.component.html',
+	styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
 
 	showRegistration: boolean = false;
 	showLogin: boolean = true;
 
-  constructor(private renderer: Renderer2) {
-	  this.renderer.setStyle(document.body, 'background', 'url("assets/images/login-background.jpg") no-repeat  fixed');
-	  this.renderer.setStyle(document.body, '-webkit-background-size', 'cover');
-	  this.renderer.setStyle(document.body, '-moz-background-size', 'cover');
-	  this.renderer.setStyle(document.body, '-o-background-size', 'cover');
-	  this.renderer.setStyle(document.body, 'background-size', 'cover');
-  }
 
-  registration(){
-	  this.showLogin = false;
-	  this.showRegistration = true;
-  }
+	constructor(private renderer: Renderer2, private router: Router) {
+		this.renderer.setStyle(document.body, 'background', 'url("assets/images/login-background.jpg") no-repeat  fixed');
+		this.renderer.setStyle(document.body, '-webkit-background-size', 'cover');
+		this.renderer.setStyle(document.body, '-moz-background-size', 'cover');
+		this.renderer.setStyle(document.body, '-o-background-size', 'cover');
+		this.renderer.setStyle(document.body, 'background-size', 'cover');
+	}
 
-  login(){
-	  this.showRegistration = false;
-	  this.showLogin = true;
-  }
+	clickRegistration() {
+		this.showLogin = false;
+		this.showRegistration = true;
+	}
 
-  ngOnInit() {
-  }
+	clickLogin() {
+		this.showRegistration = false;
+		this.showLogin = true;
+	}
+
+	logIn() {
+		window.location.href='/home'
+	}
+
+	ngOnInit() {
+	}
 
 }
